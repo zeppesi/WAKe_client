e29685f00e9976e620ca5d992ef2326e7f7449ed
unhyif <ang21icjh@gmail.com>
2024-07-05 14:23:16 +0900

    feat: shadcn-ui 및 tailwind-merge 설정, Checkbox 컴포넌트 생성

--- src/styles/utils.ts
+++ src/styles/utils.ts
@@ -1,1 +1,13 @@
-export const cn = (...classes: string[]) => classes.join(' ');
+import { type ClassValue, clsx } from 'clsx';
+import { extendTailwindMerge } from 'tailwind-merge';
+import { px0_1200_arr } from '../../tailwind.config';
+
+const merge = extendTailwindMerge<'font-size'>({
+  extend: {
+    classGroups: {
+      'font-size': [{ text: px0_1200_arr }],
+    },
+  },
+});
+
+export const cn = (...classes: ClassValue[]) => merge(clsx(classes));
