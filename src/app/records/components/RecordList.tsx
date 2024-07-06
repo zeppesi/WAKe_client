94ff88a9e4af22cdf0704452455d36df64947979
unhyif <ang21icjh@gmail.com>
2024-07-06 17:24:46 +0900

    feat: 기록 없을 시 empty 표시

--- src/app/records/components/RecordList.tsx
+++ src/app/records/components/RecordList.tsx
@@ -7,9 +7,21 @@ const RecordList = () => {
   const { records } = useRecordList();
   return (
     <section>
-      {records.map(record => (
-        <Record key={record.id} record={record} />
-      ))}
+      {records.length ? (
+        records.map(record => <Record key={record.id} record={record} />)
+      ) : (
+        <div className="flex h-400 flex-col items-center justify-center">
+          <h1 className="text-center text-40 font-bold">
+            얼마나
+            <br />
+            바빴는지
+            <br />
+            기록할 틈이
+            <br />
+            없었네요
+          </h1>
+        </div>
+      )}
     </section>
   );
 };
