7b06c97de2c858891f011e6270d430aae7296319
unhyif <ang21icjh@gmail.com>
2024-07-06 21:41:55 +0900

    chore: axios 설치

--- src/app/LayoutProvider.tsx
+++ src/app/LayoutProvider.tsx
@@ -4,7 +4,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { PropsWithChildren } from 'react';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const LayoutProvider = ({ children }: PropsWithChildren) => {
   return (
